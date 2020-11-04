390988616d28a83fe4d88b5e783a1b4bbc98bd7b
itsjay667 <73856541+itsjay667@users.noreply.github.com>
2020-11-04 14:27:28 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,5 @@
 input.onButtonPressed(Button.A, function () {
     for (let index = 0; index < 2; index++) {
-        bluetooth.advertiseUrl(
-        "https://youtu.be/Qwscb3QIVSg",
-        7,
-        true
-        )
+    	
     }
 })
